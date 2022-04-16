437ec4510891095d7fec82eef3d06bc85eb9fe34
Craggyiz <83037968+Craggyiz@users.noreply.github.com>
2022-04-15 21:34:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
+    }
+}
 
+var _createClass = function () {
+    function defineProperties(target, props) {
+        for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+        }
+    }
+    return function (Constructor, protoProps, staticProps) {
+        if (protoProps) defineProperties(Constructor.prototype, protoProps);
+        if (staticProps) defineProperties(Constructor, staticProps);
+        return Constructor;
+    };
+}();
+
+var Client = new (function () {
+    function _class1() {
+        _classCallCheck(this, _class1);
+    }
+    _createClass(_class1, [{
+        key: 'init',
+        value: function init() {
+            this.chatServer = '';
+            this.userName = '';
+        }
+    }]);
+    return _class1;
+}())();
